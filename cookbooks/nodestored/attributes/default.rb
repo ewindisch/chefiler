fb715847ecb8421949c8d6e987356e291e0ca072
Eric Windisch <eric@cloudscaling.com>
2012-07-06 11:28:50 -0400

    Add nodestored

---
+++ cookbooks/nodestored/attributes/default.rb
@@ -0,0 +1,5 @@
+default['nodestored']['root']['driver'] = 'LVM'
+default['nodestored']['swap']['driver'] = 'LVM'
+
+default['nodestored']['drivers']['lvm']['method'] = 'LVM'
+default['nodestored']['drivers']['lvm']['location'] = 'SysDisk'
---
+++ cookbooks/nodestored/recipes/default.rb
@@ -0,0 +1,10 @@
+git "/opt/chefiler/nodestored" do
+  repository "https://github.com/ewindisch/nodestored.git"
+  reference "master"
+  action "sync"
+end
+
+template "/etc/cloudinf/config.ini" do
+  mode "0400"
+  source "config.ini.erb"
+end
